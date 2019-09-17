dca78477b1e11cb741d21d4a7fe89dbfbf076f65
pouriaabbasi <abbasi.pooria@gmail.com>
2019-09-17 14:48:26 +0430

    init admin lte seprate components

--- ClientApp/src/app/app.module.ts
+++ ClientApp/src/app/app.module.ts
@@ -2,17 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppLayoutComponent } from './_layouts/app-layout/app-layout.component';
+import { MainHeaderComponent } from './_components/main-header/main-header.component';
+import { MainSidebarComponent } from './_components/main-sidebar/main-sidebar.component';
+import { MainFooterComponent } from './_components/main-footer/main-footer.component';
+import { ControlSidebarComponent } from './_components/control-sidebar/control-sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppLayoutComponent,
+    MainHeaderComponent,
+    MainSidebarComponent,
+    MainFooterComponent,
+    ControlSidebarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppLayoutComponent]
 })
 export class AppModule { }
