f2383542e56f5a712cffe5856e7d9d71e3347950
pouriaabbasi <abbasi.pooria@gmail.com>
2019-09-19 00:28:38 +0430

    logout

--- ClientApp/src/app/_components/main-header/main-header.component.ts
+++ ClientApp/src/app/_components/main-header/main-header.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SecurityService } from 'src/app/services/security.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-main-header',
@@ -11,11 +12,17 @@ export class MainHeaderComponent implements OnInit {
   fullName = '';
 
   constructor(
-    private securityService: SecurityService
+    private securityService: SecurityService,
+    private router: Router
   ) { }
 
   ngOnInit() {
     this.fullName = `${this.securityService.currentUserValue.firstName} ${this.securityService.currentUserValue.lastName}`;
+  }
+
+  public logout() {
+    this.securityService.logout();
+    this.router.navigate(['/login']);
   }
 
 }
